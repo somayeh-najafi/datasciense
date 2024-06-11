8071a141e4721a694c6efcdb5d6a8cc8bbe6b2da
somayeh-najafi <59369443+somayeh-najafi@users.noreply.github.com>
2024-06-11 12:39:25 -0600

    updating path

--- Projects/8-RegulatorySolution/Deployment/Streamlit/app.py
+++ Projects/8-RegulatorySolution/Deployment/Streamlit/app.py
@@ -11,7 +11,7 @@ import seaborn as sns
 st.set_page_config(page_title='Regulatory Dashboard!',page_icon=":bar_chart:",layout='wide')
 
 #importing model
-model = pickle.load(open('RiskPredictor_RF_V2.pkl','rb'))
+model = pickle.load(open('Projects/8-RegulatorySolution/Deployment/Streamlit/model/RiskPredictor_RF_V2.pkl','rb'))
 #model = joblib.load('Projects/8-RegulatorySolution/Deployment/Streamlit/model/RiskPredictor_RF_V2.pkl')
 
 #Encoding Function
