3b31bed44b5d460f733e9c3c3426d32cfea44704
somayeh-najafi <59369443+somayeh-najafi@users.noreply.github.com>
2024-06-22 17:04:25 -0600

    model path update

--- Projects/8-RegulatorySolution/Deployment/Streamlit/RiskAnalysis.py
+++ Projects/8-RegulatorySolution/Deployment/Streamlit/RiskAnalysis.py
@@ -11,7 +11,7 @@ import seaborn as sns
 st.set_page_config(page_title='Regulatory Dashboard!',page_icon=":bar_chart:",layout='wide')
 
 #importing model
-model = joblib.load('RiskPredictor_RF_V3.pkl')
+model = joblib.load('Projects/8-RegulatorySolution/Deployment/Streamlit/model/RiskPredictor_RF_V3.pkl')
 
 #Encoding Function
 def encoding(item):
