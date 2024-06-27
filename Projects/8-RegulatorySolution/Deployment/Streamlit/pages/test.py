8a0725a59bb10e7d8bf3d875f4d231fd3ff9b6fb
somayeh-najafi <59369443+somayeh-najafi@users.noreply.github.com>
2024-06-27 09:58:36 -0600

    Delete test.py

--- Projects/8-RegulatorySolution/Deployment/Streamlit/pages/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import streamlit as st
-
-uploaded_file = pd.read_csv('Projects/8-RegulatorySolution/Deployment/Streamlit/Test_Dataset_E100.csv')
-uploaded_file=uploaded_file.fillna('None')
-st.write(uploaded_file)
